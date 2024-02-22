f3bef2d69171899e07da97977ff76947a964c8d5
MuhammadHARIS46 <muhammadhariskuk123@gmail.com>
2024-02-22 15:53:58 +0500

    authentication using clerk sidebar for desktop and navbar for mobile

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Sidebar from "@/components/shared/Sidebar";
+import MobileNav from "@/components/shared/MobileNav"
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="root">
+      <Sidebar/>
+      <MobileNav />
+      <div className="root-container">
+        <div className="wrapper">{children}</div>
+      </div>
+    </main>
+  );
+};
+
+export default Layout;
